619753250c5d1587c447ceaa896a365a0600a592
Towelie <mail@towelie.dev>
2025-04-08 19:59:14 +0200

    Added nohlsearch reset, added diagnostics

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,11 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
 vim.keymap.set("v", "<leader>cs", ":sort<CR>")
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, { desc = '[D]iagnositcs Up' })
+vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, { desc = '[D]iagnositcs Down' })
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = '[D]iagnositcs Float' })
+vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = '[D]iagnositcs List' })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
